eb1a59f0cce78a04063f0c220f8555038e5805b9
valies <da_valies@hotmail.com>
2024-12-02 23:46:54 +0100

    move input files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.day_02.solution import day_02_part_1 as day_02_part_1
 from src.day_02.solution import day_02_part_2 as day_02_part_2
 
 if __name__ == '__main__':
-    day_01_part_1("./src/day_01/input.txt")
-    day_01_part_2("./src/day_01/input.txt")
-    day_02_part_1("./src/day_02/input.txt")
-    day_02_part_2("./src/day_02/input.txt")
+    day_01_part_1("input/day_01_input.txt")
+    day_01_part_2("input/day_01_input.txt")
+    day_02_part_1("input/day_02_input.txt")
+    day_02_part_2("input/day_02_input.txt")
