01589b1dd41a275302e6c25fc5209f906d792223
valies <da_valies@hotmail.com>
2024-12-02 23:08:44 +0100

    cleanup

--- tests/day_01/test_day_01.py
+++ tests/day_01/test_day_01.py
@@ -5,15 +5,13 @@ from src.day_01.solution import day_01_part_1, day_01_part_2
 
 
 class MyTestCases(unittest.TestCase):
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
     def test_day_01_part1(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        actual_result = day_01_part_1(current_dir + '/input.txt')
-        self.assertEqual(11, actual_result)
+        self.assertEqual(11, day_01_part_1(self.CURRENT_DIR + '/input.txt'))
 
     def test_day_01_part2(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        actual_result = day_01_part_2(current_dir + '/input.txt')
-        self.assertEqual(31, actual_result)
+        self.assertEqual(31, day_01_part_2(self.CURRENT_DIR + '/input.txt'))
 
 
 if __name__ == '__main__':
--- tests/day_02/test_day_02.py
+++ tests/day_02/test_day_02.py
@@ -5,15 +5,13 @@ from src.day_02.solution import day_02_part_1, day_02_part_2
 
 
 class MyTestCases(unittest.TestCase):
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
     def test_day_02_part1(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        actual_result = day_02_part_1(current_dir + '/input.txt')
-        self.assertEqual(2, actual_result)
+        self.assertEqual(2, day_02_part_1(self.CURRENT_DIR + '/input.txt'))
 
     def test_day_02_part2(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        actual_result = day_02_part_2(current_dir + '/input.txt')
-        self.assertEqual(4, actual_result)
+        self.assertEqual(4, day_02_part_2(self.CURRENT_DIR + '/input.txt'))
 
 
 if __name__ == '__main__':
