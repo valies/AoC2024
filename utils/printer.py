9d765e999869d919e33e57cc5aa7f3bb7d0e50e7
valies <da_valies@hotmail.com>
2024-12-02 23:15:47 +0100

    changed duration from seconds to ms

--- utils/printer.py
+++ utils/printer.py
@@ -2,14 +2,14 @@ import time
 
 
 def pretty_print(day, result, duration):
-    print(f"{str(day)}: {str(result)} \u2b50 ({duration} seconds)")
+    print(f"{str(day)}: {str(result)} \u2b50 ({duration} ms)")
 
 
 def timing_decorator(func):
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
-        duration = time.perf_counter() - start_time
+        duration = (time.perf_counter() - start_time) * 1000
         pretty_print(func.__name__, result, duration)
         return result
 
